c997260c6b4978691067d24fdd8fb89f9a4318b1
DiMaggio <vluxx17@gmail.com>
2026-07-19 18:10:44 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+BOT_TOKEN = "8900768249:AAHy_p_pGpcZTugbU1UnmRti-thQqln7HS4"
+ADMIN_ID = 8297446667
+
+# Внешние API ключи (замените на свои)
+VK_API_TOKEN = "ваш_vk_service_token"   # получить в VK API
+IP_API_KEY = "ваш_ключ_для_ip-api.com"  # можно без ключа, но с ограничениями
+WHOIS_API_KEY = "ваш_whois_api"         # например whoisapi.com
